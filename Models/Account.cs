6417ad1c8b5a2e284d0187574a7f5ebe18b56a07
tanlongEIUK8 <long.truong.set18@eiu.edu.vn>
2022-06-14 21:35:07 +0700

    update account validation/ regex

--- Models/Account.cs
+++ Models/Account.cs
@@ -4,13 +4,26 @@ namespace EcommercialWebApplication.Models
 {
     public class Account
     {
+        [RegularExpression(@"^[a-z_0-9]+$", ErrorMessage = "Username contains letters a-z, numbers 0-9 and no spaces or symbols")]
+        [Required(ErrorMessage = "Username is required")]
         public string UserName { get; set; }
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+           ErrorMessage = "Password must contain at least eight characters, at least one number and both lower and uppercase letters and special characters")]
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
-        public string CurrentPassword { get; set; }
+
         public bool RememberMe { get; set; }
         [EmailAddress]
+
+        [RegularExpression(@"^[a-z0-9]+@[a-z]+(\.[a-z]+)+$",
+            ErrorMessage = "Email is not valid")]
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
+        [RegularExpression(@"^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$",
+            ErrorMessage = "Wrong phone number format")]
+        [Required(ErrorMessage = "Phone number is required")]
         public string PhoneNumber { get; set; }
+        public string CurrentPassword { get; set; }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let password = document.getElementById("register-password");
+let repeatPass = document.getElementById("register-repeat-password");
+let errorSpan = document.getElementById("repeat-pasword-error");
+errorSpan.style.display = "none";
+let btn = document.getElementById("registerSubmit");
+console.log(repeatPass.nodeValue);
+
+function checkRepeat() {
+    if (repeatPass.value !== password.value) {
+        errorSpan.style.display = "block";
+        btn.disabled = true;
+
+    } else {
+        errorSpan.style.display = "none";
+        btn.disabled = false;
+    }
+}
